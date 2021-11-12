9844ab85194d62d21fe56ba8dede78f0e64e7e84
Vu Minh Tu <tu.vm41@gmail.com>
2021-11-12 10:51:41 +0700

    Add simple math

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  add,
+  subtract,
+  divide,
+  multify,
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multify(a, b) {
+  return a * b;
+}
